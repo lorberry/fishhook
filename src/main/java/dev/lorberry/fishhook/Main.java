48bdee77d8eb159c3c09b836175ed27ae622efc8
Lars <lorbeeren@proton.me>
2025-05-08 20:33:19 +0200

    Initial commit

---
+++ src/main/java/dev/lorberry/fishhook/Main.java
@@ -0,0 +1,10 @@
+package dev.lorberry.fishhook;
+
+import java.lang.instrument.Instrumentation;
+
+public class Main {
+
+    public static void agentmain(String agentArgs, Instrumentation inst) {
+
+    }
+}
