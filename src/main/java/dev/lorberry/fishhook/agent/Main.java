20d98b1d1fc3b5071aa6927c7a6c7d4df1d1f935
Lars <lorbeeren@proton.me>
2025-05-09 02:44:24 +0200

    Update Main.java

--- src/main/java/dev/lorberry/fishhook/agent/Main.java
+++ src/main/java/dev/lorberry/fishhook/agent/Main.java
@@ -13,64 +13,68 @@ import java.util.jar.JarFile;
 public class Main {
 
     public static void agentmain(String agentArgs, Instrumentation inst) throws URISyntaxException, IOException, NoSuchMethodException, InvocationTargetException, IllegalAccessException {
-        System.out.println("Started Fish agent (Loaded classes: " + inst.getAllLoadedClasses().length + ")");
+        try {
+            System.out.println("Started Fish agent (Loaded classes: " + inst.getAllLoadedClasses().length + ")");
 
-        FishClassLoader fCl = null;
-        // minecraft class loader
-        ClassLoader mcCl = null;
-        Class<?> mcClC = null;
+            FishClassLoader fCl = null;
+            // minecraft class loader
+            ClassLoader mcCl = null;
+            Class<?> mcClC = null;
 
-        for (Class<?> c : inst.getAllLoadedClasses()) {
-            if (c.getTypeName().startsWith("net.minecraft")) {
-                mcCl = c.getClassLoader();
-                mcClC = mcCl.getClass();
-                fCl = new FishClassLoader(mcCl);
-                System.out.println("Attached Fish agent");
-                break;
+            for (Class<?> c : inst.getAllLoadedClasses()) {
+                if (c.getTypeName().startsWith("net.minecraft")) {
+                    mcCl = c.getClassLoader();
+                    mcClC = mcCl.getClass();
+                    fCl = new FishClassLoader(mcCl);
+                    System.out.println("Attached Fish agent");
+                    break;
+                }
             }
-        }
 
-        if (fCl == null) {
-            throw new RuntimeException("Minecraft class loader could not be found");
-        }
-
-        JarFile jarFile = FishClassLoader.getJar();
-        ArrayList<JarEntry> entriesToLoad = new ArrayList<>();
-
-        for (JarEntry file : jarFile.stream().toList()) {
-            if (file.getName().endsWith(".class") && file.getName().startsWith("dev/lorberry/fishhook")) {
-                entriesToLoad.add(file);
+            if (fCl == null) {
+                throw new RuntimeException("Minecraft class loader could not be found");
             }
-        }
 
-        Method defineClassMethod = mcClC.getMethod("defineClassFwd", String.class, byte[].class, int.class, int.class, CodeSource.class);
-        defineClassMethod.setAccessible(true);
+            JarFile jarFile = FishClassLoader.getJar();
+            ArrayList<JarEntry> entriesToLoad = new ArrayList<>();
 
-        while (!entriesToLoad.isEmpty()) {
-            ArrayList<JarEntry> failed = new ArrayList<>();
+            for (JarEntry file : jarFile.stream().toList()) {
+                if (file.getName().endsWith(".class") && file.getName().startsWith("dev/lorberry/fishhook")) {
+                    entriesToLoad.add(file);
+                }
+            }
 
-            for (JarEntry file : entriesToLoad) {
-                byte[] classBytes = jarFile.getInputStream(file).readAllBytes();
-                String className = file.getName().replace("/", ".").replace(".class", "");
+            Method defineClassMethod = mcClC.getMethod("defineClassFwd", String.class, byte[].class, int.class, int.class, CodeSource.class);
+            defineClassMethod.setAccessible(true);
 
-                try {
-                    defineClassMethod.invoke(mcCl, className, classBytes, 0, classBytes.length, null);
-                } catch (IllegalAccessException | InvocationTargetException e) {
-                    if (e.getCause() instanceof LinkageError) {
-                        failed.add(file);
+            while (!entriesToLoad.isEmpty()) {
+                ArrayList<JarEntry> failed = new ArrayList<>();
+
+                for (JarEntry file : entriesToLoad) {
+                    byte[] classBytes = jarFile.getInputStream(file).readAllBytes();
+                    String className = file.getName().replace("/", ".").replace(".class", "");
+
+                    try {
+                        defineClassMethod.invoke(mcCl, className, classBytes, 0, classBytes.length, null);
+                    } catch (IllegalAccessException | InvocationTargetException e) {
+                        if (e.getCause() instanceof LinkageError) {
+                            failed.add(file);
+                        } else {
+                            throw e;
+                        }
+                    }
+
+                    if (failed.size() == entriesToLoad.size()) {
+                        throw new RuntimeException("Failed to load any classes");
                     } else {
-                        throw e;
+                        entriesToLoad = failed;
                     }
                 }
-
-                if (failed.size() == entriesToLoad.size()) {
-                    throw new RuntimeException("Failed to load any classes");
-                } else {
-                    entriesToLoad = failed;
-                }
             }
-        }
 
-        Fish.init();
+            Fish.init();
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
     }
 }
